42d60ff18f11d3b6724ca68ed86ad472d4477a8a
Wilfredo Rodriguez <wilfredor@gmail.com>
2023-01-05 22:02:04 -0500

    Migration to OOP

--- src/enemy.ts
+++ src/enemy.ts
@@ -1,39 +1,36 @@
 
-import { Config } from "./config";
-import { Enemies } from "./enemies";
+
+import { IGame } from "./modele/igame";
+import {IEnemy} from "./modele/ienemy";
 import { Game } from "./game";
-export class Enemy {
+export class Enemy implements IEnemy {
    height: number;
    width: number;
    x: number;
    y: number;
    index: number;
    img: HTMLImageElement;
-   enemies: Enemies;
    game: Game;
-   config: Config;
-   constructor(x: number, y: number, index: number, enemyType: HTMLImageElement, enemies: Enemies) {
-      this.width = enemies.width;
-      this.height = enemies.height;
+   constructor(x: number, y: number, index: number, enemyType: HTMLImageElement, game: Game) {
+      this.width = game.config.enemyWidth;
+      this.height = game.config.enemyHeight;
       this.x = x;
       this.y = y;
       this.index = index;
       this.img = enemyType;
-      this.enemies = enemies;
-      this.game = this.enemies.game;
-      this.config = this.enemies.config;
+      this.game = game;
       this.paint();
    }
 
    paint() {
-      this.config.context.drawImage(this.img, this.x, this.y, this.width, this.height);
+      this.game.config.context.drawImage(this.img, this.x, this.y, this.width, this.height);
    }
 
    Obstruction() {
-      var elementNumber = this.enemies.element.length - 1;
+      var elementNumber = this.game.enemies.element.length - 1;
       for (var i = 0; i <= elementNumber; i++) {
-         if ((this.enemies.element[i].x == this.x) && 
-             (this.enemies.element[i].index > this.index))
+         if ((this.game.enemies.element[i].x == this.x) && 
+             (this.game.enemies.element[i].index > this.index))
             return true;
       }
       return false;
@@ -41,37 +38,38 @@ export class Enemy {
 
    //Enemy fire
    fire() {
-      if (!this.game.paused)
-         this.directionFire(this.x, this.y, this);
+      if (!this.game.paused) {
+         this.directionFire(this.x, this.y);
+      }
    };
 
-   //Fire direction
-   directionFire(xPos: number, i: number, element: this) {
-      setTimeout(() => {
-         if (i <= this.config.canvas.height - 20) {//If the fire is not in screen border	
-            //Make a fire and delete track
-            this.config.context.fillStyle = "#FF0000";
-            this.config.context.clearRect(xPos, i - 20, 3, 9);
-            this.config.context.fillRect(xPos, i, 3, 9);
-            this.config.context.fillStyle = "#7fff00";
-            //the fire resume trayectory
-            element.directionFire(xPos, i + 20, element);
-         } else {
-            if ((xPos >= this.enemies.nave.x) && (xPos <= (this.enemies.nave.x + this.width))) {
-               this.enemies.nave.life--;
-               this.config.life = this.enemies.nave.life;
-               if (this.enemies.nave.life <= 0) {
-                  this.game.showMessage("You are dead");
-                  setTimeout(function () {
-                     window.location.reload();
-                  }, 3000);
+   _makefire(context:CanvasRenderingContext2D, i:number, xPos:number) {
+      //Make a fire and delete track
+      context.fillStyle = "#FF0000";
+      context.clearRect(xPos, i - 20, 3, 9);
+      context.fillRect(xPos, i, 3, 9);
+      context.fillStyle = "#7fff00";
+   }
 
-               } else if (this.enemies.nave.life===1) {
-                  alert("You have only "+this.enemies.nave.life+" life");
+   //Fire direction
+   directionFire(xPos: number, i: number) {
+      setTimeout(() => {
+         if (i <= this.game.config.canvas.height - 20) {//If the fire is not in screen border	
+            //Make a fire and delete track
+            this._makefire(this.game.config.context, i, xPos);
+            //the fire resume trayectory
+            this.directionFire(xPos, i + 20);
+         } else if ((xPos >= this.game.nave.x) && (xPos <= (this.game.nave.x + this.width))) {
+               this.game.nave.life--;
+               this.game.config.life = this.game.nave.life;
+               if (this.game.nave.life <= 0) {
+                  this.game.showMessage("You are dead");
+                  this.game.reload();
+               } else if (this.game.nave.life===1) {
+                  alert(`You have only ${this.game.nave.life} life`);
                }
-            } else
-            this.config.context.clearRect(xPos, i - 20, 3, 9);
-         }
+         } else
+            this.game.config.context.clearRect(xPos, i - 20, 3, 9);
       }, 30);
    };
 
